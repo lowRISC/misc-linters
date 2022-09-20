5b62fe89ecf83ccdffe3bb8cb7a940ceda67da76
Chris Frantz <cfrantz@google.com>
2022-09-20 14:37:45 -0700

    Create a `licence_test` rule
    The `license_test` rule executes the licence checker as a bazel test
    (rather than an executable), thus allowing an upstream project to
    use the rule as part of a set of presubmit tests.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- rules/licence-checker-runner.template.sh
+++ rules/licence-checker-runner.template.sh
@@ -3,8 +3,16 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-if ! cd "$BUILD_WORKSPACE_DIRECTORY"; then
-    echo "Unable to change to workspace (BUILD_WORKSPACE_DIRECTORY: ${BUILD_WORKSPACE_DIRECTORY})"
+WORKSPACE="@@WORKSPACE@@"
+
+if [[ ! -z "${WORKSPACE}" ]]; then
+    REPO="$(dirname "$(realpath ${WORKSPACE})")"
+    cd ${REPO} || exit 1
+elif [[ ! -z "${BUILD_WORKSPACE_DIRECTORY+is_set}" ]]; then
+    cd ${BUILD_WORKSPACE_DIRECTORY} || exit 1
+else
+    echo "Neither WORKSPACE nor BUILD_WORKSPACE_DIRECTORY were set."
+    echo "If this is a test rule, add 'workspace = \"//:WORKSPACE\"' to your rule."
     exit 1
 fi
 
